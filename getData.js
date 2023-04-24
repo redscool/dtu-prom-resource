7815ff6f9f3e37d8188e21d27c7f6824b3edf794
asifkhan067 <askhan.ak912@gmail.com>
2023-04-25 01:07:52 +0530

    decreased server load

--- getData.js
+++ getData.js
@@ -1,37 +1,37 @@
-import Male from './model/Male.js';
-import Female from './model/Female.js';
+import Male from "./model/Male.js";
+import Female from "./model/Female.js";
 
-const TIME_INTERVAL = 1000 * 2;
+const TIME_INTERVAL = 1000 * 20;
 const prev = {
-    "M": 0,
-    "F": 0,
+  M: 0,
+  F: 0,
 };
 
 const loading = {
-    "M": false,
-    "F": false,
+  M: false,
+  F: false,
 };
 
 export default async function getData(CACHE, type) {
-    const time = Date.now();
-    const prevTime = prev[type];
+  const time = Date.now();
+  const prevTime = prev[type];
 
-    if ((time - prevTime < TIME_INTERVAL) || loading[type]) {
-        console.log("Cache hit for " + type);
-        return CACHE[type];
-    } else {
-        loading[type] = true;
+  if (time - prevTime < TIME_INTERVAL || loading[type]) {
+    console.log("Cache hit for " + type);
+    return CACHE[type];
+  } else {
+    loading[type] = true;
 
-        const Model = type === "M" ? Male : Female;
+    const Model = type === "M" ? Male : Female;
 
-        console.log("DB hit for " + type);
+    console.log("DB hit for " + type);
 
-        const data = await Model.find({ regComplete: true });
+    const data = await Model.find({ regComplete: true });
 
-        prev[type] = time;
-        CACHE[type] = data;
-        loading[type] = false;
+    prev[type] = time;
+    CACHE[type] = data;
+    loading[type] = false;
 
-        return data;
-    }
+    return data;
+  }
 }
